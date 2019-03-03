cfce08a9d8ab618b248b1c7d67094086351d4472
74q <892865384@qq.com>
2019-03-03 12:55:08 +0800

    Initial commit

---
+++ src/main/java/com/twc/uu/helloworld/UUApp.java
@@ -0,0 +1,12 @@
+package com.twc.uu.helloworld;
+
+import javafx.application.Application;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UUApp {
+    public static void main(String[] args) {
+        SpringApplication.run(UUApp.class, args);
+    }
+}
